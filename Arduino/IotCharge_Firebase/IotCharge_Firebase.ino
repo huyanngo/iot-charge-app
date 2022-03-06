79ce66b5b05cd10c58b3fc4be065ef5afd16e3ad
huyanngo <huyanngo2909@gmail.com>
2022-03-06 17:31:38 +0700

    Update Fingerprint for Arduino

--- Arduino/IotCharge_Firebase/IotCharge_Firebase.ino
+++ Arduino/IotCharge_Firebase/IotCharge_Firebase.ino
@@ -1,5 +1,5 @@
 #include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include <FirebaseESP8266.h>
 
 // Connect to Firebase and Wifi.
 #define FIREBASE_HOST "iotcharger-f97bc-default-rtdb.firebaseio.com"
@@ -8,6 +8,7 @@
 #define WIFI_PASSWORD "20052111"
 
 int statusD1 = 0;
+FirebaseData firebaseData;
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,20 +27,26 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  pinMode(D1,OUTPUT)
+  pinMode(D1,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   //Repeatedly check the Status of Device 1
-  statusD1 = Firebase.getInt("Device/1/status");
-  
-  if(statusD1 == 0){
-    Serial.println("----> IoT Charge 1 OFF")
-    digitalWrite(D1,LOW)
-  }else if(statusD1 == 1){
-    Serial.println("----> IoT Charge 1 ON")
-    digitalWrite(D1,HIGH)
+  if (Firebase.getInt(fb,"Device/1/status")){
+    if(fb.dataType() == "int"){
+      statusD1 = (fb.intData());
+      if(statusD1 == 0){
+        Serial.println("----> IoT Charge 1 OFF");
+        digitalWrite(D1,LOW);
+      }else if(statusD1 == 1){
+        Serial.println("----> IoT Charge 1 ON");
+        digitalWrite(D1,HIGH);
+      }
+    }
+  }else{
+    Serial.println(fb.errorReason());
+    Serial.println("Failed");
   }
 }
